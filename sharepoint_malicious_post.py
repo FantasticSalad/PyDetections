46a8a91f040025d7be792ae646769d5fba2f7357
FantasticSalad <jamespatrickbullen@gmail.com>
2025-10-13 21:32:53 +1100

    added mfa registration and ssl log files, completed suspicious webshell detection, started sharepoint detection and created detection template

---
+++ sharepoint_malicious_post.py
@@ -0,0 +1,16 @@
+# Title: SharePoint Vulnerability CVE-2025-53771 Attempted Exploit - Malicious Post Request for Auth Bypass
+# Description: 
+# MITRE Tactic: 
+# MITRE Technique: 
+# Log Source: 
+# Code:
+
+# with open("Logs\\ssl_toolpane.txt", "r") as logs:
+#     for line in logs:
+#         line_data = line.splitlines()
+#     print(line_data)
+
+with open("Logs\\ssl_toolpane.txt", "r") as logs:
+    liney = logs.splitlines()
+    print(liney)
+    
--- suspicious_webshell.py
+++ suspicious_webshell.py
@@ -1,13 +1,24 @@
-# Title: 
-# Description:
-# MITRE Tactic:
-# MITRE Technique:
-# Log Source:
+# Title: Suspicious Webshell Spawned
+# Description: Detects suspicious child processes spawned by w3wp.exe, indicating potential webshell activity.
+# MITRE Tactic: Execution
+# MITRE Technique: T1059 - Command and Scripting Interpreter
+# Log Source: Windows Process Logs
 # Code:
 
 with open("Logs\\win_process_logs.txt", "r") as logs:
     for line in logs:
-        data = line.split(",")
-        if data[4] in ("cmd.exe", "powershell.exe", "wscript.exe", "cscript.exe", "mshta.exe", "pwsh.exe", "wmic.exe", "bitsadmin.exe"):
-            print(data)
+        data = line.strip().split(",")
+        if len(data) > 5: # check parent_process exists
+            timestamp = data[0]
+            host = data[1]
+            user = data[2]
+            child_process = data[3]
+            # command_line = data[4]
+            parent_process = data[5]
+            # parent_command_line = data[6]
+            # path = data[7]
+            # network_activity = data[8]
+            if parent_process.lower() == "w3wp.exe" and child_process.lower() in ("cmd.exe", "powershell.exe", "wscript.exe", "cscript.exe", "mshta.exe", "pwsh.exe", "wmic.exe", "bitsadmin.exe","wevtutil.exe","whoami.exe"):
+                print(f"{timestamp}: User {user} utilised {parent_process} to spawn {child_process}, indicating potential suspicious webshell activity on host {host}")
+
         
