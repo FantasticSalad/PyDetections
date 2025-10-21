ccf6599693407f591abefd6da94ca39a02c84cf1
FantasticSalad <jamespatrickbullen@gmail.com>
2025-10-21 22:45:16 +1100

    progressed local admin creation

--- local_admin_creation.py
+++ local_admin_creation.py
@@ -1,4 +1,4 @@
-# Title: 
+# Title: Local Admin Creation
 # Description: 
 # MITRE Tactic: 
 # MITRE Technique: 
@@ -7,17 +7,35 @@
 
 import re
 
+accounts_created = []
+
+
 with open("Logs\\win_process.txt", "r") as log_file:
     for line in log_file:
         i_list = line.strip().split(",")
         if len(i_list) < 7:
             continue
         timestamp, host, user, child_process, command_line, parent_process, path = i_list[:7]
-        net_check_match = re.search(r"\^net", command_line)
-        if net_check_match:
-            print(i_list)
+        command_line = command_line.strip("\"")
+        net_user_match = re.search(r"net1?\suser\s(\S+)", command_line)
+        # print(command_line)
+        if net_user_match:
+            accounts_created.append(net_user_match.group(1))
+        # if net_user_match:
+        #     print(i_list)
+    for line in log_file:
+        i_list_a = line.strip().split(",")
+        if len(i_list_a) < 7:
+            continue
+        timestamp, host, user, child_process, command_line, parent_process, path = i_list[:7]
+        command_line = command_line.strip("\"")
+        add_admin_match = re.search(r"net1?\slocalgroup\sadministrators\s(\S+)", command_line)
+        print(add_admin_match)
+        # if add_admin_match.group(1) in accounts_created:
+        #     print(i_list_a)
         
         
+# print(accounts_created)
     
 
 
