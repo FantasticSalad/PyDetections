244d7c6b09b48fdd0834533eb520456ec70b6319
FantasticSalad <jamespatrickbullen@gmail.com>
2025-11-15 20:35:33 +1100

    certutil updates

--- Detections/Draft/certutil_abuse.py
+++ Detections/Draft/certutil_abuse.py
@@ -5,4 +5,14 @@
 # Log Source: 
 # Code:
 
-with open
+import json
+import pprint
+import re
+
+with open("logs/win_process_json.txt", "r") as log_file:
+    json_data = json.loads(log_file.read())
+    pprint.pprint(json_data)
+    for log in json_data:
+        certutil_check = re.search("(?i)certutil",log["process"]["name"])
+        cmdline_check = re.search("(?i)certutil.+urlcache.+?(https?:\/\/\S+)\s+(\S+)",log["process"]["command_line"])
+        print(cmdline_check.group)
