8a72895b7e443cc313a96fcbcfe67795af9a0418
FantasticSalad <jamespatrickbullen@gmail.com>
2025-11-16 19:02:47 +1100

    completed certutil abuse

--- Detections/Draft/certutil_abuse.py
+++ Detections/Draft/certutil_abuse.py
@@ -1,18 +1,22 @@
-# Title: Certutil
-# Description: 
-# MITRE Tactic: 
-# MITRE Technique: 
-# Log Source: 
+# Title: Suspicious File Download via Certutil URLCache
+# Description: Detects suspicious use of certutil.exe to download files via the `-urlcache` option, often leveraged by adversaries for malware delivery or staging.
+# MITRE Tactic: Command and Control
+# MITRE Technique: T1105 – Ingress Tool Transfer
+# Log Source: Windows Process Creation
 # Code:
 
+
 import json
-import pprint
 import re
 
 with open("logs/win_process_json.txt", "r") as log_file:
     json_data = json.loads(log_file.read())
-    pprint.pprint(json_data)
     for log in json_data:
+        timestamp = log["@timestamp"]
+        host = log["computer_name"]
+        user = log["user"]["name"]
+        
         certutil_check = re.search("(?i)certutil",log["process"]["name"])
         cmdline_check = re.search("(?i)certutil.+urlcache.+?(https?:\/\/\S+)\s+(\S+)",log["process"]["command_line"])
-        print(cmdline_check.group)
+        if certutil_check and cmdline_check:
+            print(f"{timestamp}: User {user} utilised certutil.exe to download {cmdline_check.group(2)} from URL {cmdline_check.group(1)}, indicating potential suspicious activity on host {host}")
