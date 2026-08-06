5064c53c56f00293dcc6cf9bf888081bda39e4d0
FantasticSalad <jamespatrickbullen@gmail.com>
2026-08-06 21:36:17 +1000

    mfa detection

---
+++ Detections/Draft/multiple_mfa_device.py
@@ -0,0 +1,16 @@
+# Title: Multiple MFA Device Registration 
+# Description: 
+# MITRE Tactic: 
+# MITRE Technique: 
+# Log Source: 
+# Code:
+
+import re
+import os
+
+with open("Logs/mfa_registration.txt", "r") as data:
+    for line in data:
+            for word in line:
+                line_list = line.strip().split()
+            print(line_list)
+    
